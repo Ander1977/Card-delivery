ef9d1c85cfd0a009ddaa95f1f5825c88774b212c
Ander1977 <kopa.andrej@gmail.com>
2020-07-09 10:11:31 +0300

    Added new test

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,6 +1,4 @@
 
-
-import com.codeborne.selenide.ElementsCollection;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
@@ -22,9 +20,70 @@ public class CardDeliveryTest {
         $$("button").find(exactText("Забронировать")).click();
         $(byText("Забронировать")).waitUntil(visible,15000);
         $(withText("Успешно!"));
-
-
     }
+
+    @Test
+    void shouldPhoneNotCorrect(){
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Хабаровск");
+        $("[data-test-id='date'] input").setValue("localDate+3");
+        $("[data-test-id='name'] input").setValue("Копатилов Андрей");
+        $("[data-test-id='phone'] input").setValue("89098765432");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id='phone']").shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id='phone']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+    }
+
+    @Test
+    void shouldNameNotCorrect(){
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Хабаровск");
+        $("[data-test-id='date'] input").setValue("localDate+3");
+        $("[data-test-id='name'] input").setValue("Kopatilov Andrey");
+        $("[data-test-id='phone'] input").setValue("+79098765432");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id='name']").shouldHave(text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id='name']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+    }
+
+    @Test
+    void shouldChecboxNotClick(){
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Хабаровск");
+        $("[data-test-id='date'] input").setValue("localDate+3");
+        $("[data-test-id='name'] input").setValue("Копатилов Андрей");
+        $("[data-test-id='phone'] input").setValue("+79098765432");
+        $$("button").find(exactText("Забронировать")).click();
+        $("[class='checkbox__text']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+    }
+
+    @Test
+    void shouldCityNotSelected(){
+        open("http://localhost:9999");
+        $("[data-test-id='date'] input").setValue("localDate+3");
+        $("[data-test-id='name'] input").setValue("Копатилов Андрей");
+        $("[data-test-id='phone'] input").setValue("+79098765432");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id='city']").shouldHave(text("Поле обязательно для заполнения"));
+        $("[data-test-id='city']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+    }
+
+    @Test
+    void shouldCityNotUnavailable(){
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Бикин");
+        $("[data-test-id='date'] input").setValue("localDate+3");
+        $("[data-test-id='name'] input").setValue("Копатилов Андрей");
+        $("[data-test-id='phone'] input").setValue("+79098765432");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id='city']").shouldHave(text("Доставка в выбранный город недоступна"));
+        $("[data-test-id='city']").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
+    }
+
 
 }
 
